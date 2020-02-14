10548630f6ab5a8ca578fbd69cd58bb053df977f
Maciej Kopeć <dimitor115@gmail.com>
2020-02-14 19:47:51 +0100

    Improve dynamic query api definition

--- apiGenerator.ts
+++ apiGenerator.ts
@@ -1,4 +1,4 @@
-import { generateRoutes, generateSwaggerSpec, RoutesConfig, SwaggerConfig } from 'tsoa'
+import {generateRoutes, generateSwaggerSpec, RoutesConfig, SwaggerConfig} from 'tsoa'
 import ts from 'ttypescript/lib/typescript'
 
 (async () => {
@@ -12,43 +12,127 @@ import ts from 'ttypescript/lib/typescript'
         controllerPathGlobs: ['./src/api/*'],
         securityDefinitions: {
             GOOGLE_TOKEN: {
-                type: "apiKey",
-                name: "authorization",
-                in: "header"
+                type: 'apiKey',
+                name: 'authorization',
+                in: 'header'
             }
         },
-        "specMerging": "recursive",
-        "spec": {
-            "paths": {
-                "/events": {
-                    "post": {
-                        "consumes": [
-                            "multipart/form-data"
+        'specMerging': 'recursive',
+        'spec': {
+            'paths': {
+                '/events/{eventId}/forms/{id}/participants': {
+                    'patch': {
+                        'parameters': [
+                            {
+                                'in': 'query',
+                                'name': 'filter',
+                                'schema': {
+                                    'type': 'object'
+                                },
+                                'style': 'form',
+                                'explode': true
+                            },
+                            {
+                                'description': 'event id',
+                                'in': 'path',
+                                'name': 'eventId',
+                                'required': true,
+                                'schema': {
+                                    'type': 'string'
+                                }
+                            },
+                            {
+                                'description': 'form id',
+                                'in': 'path',
+                                'name': 'id',
+                                'required': true,
+                                'schema': {
+                                    'type': 'string'
+                                }
+                            }
+                        ]
+                    }
+                },
+
+                '/events/{eventId}/forms/{id}': {
+                    'get': {
+                        'parameters': [
+                            {
+                                'in': 'query',
+                                'name': 'query',
+                                'schema': {
+                                    'type': 'object',
+                                    'properties': {
+                                        'page': {
+                                            'type': 'string'
+                                        },
+                                        'count': {
+                                            'type': 'string'
+                                        },
+                                        'sort': {
+                                            'type': 'object'
+                                        },
+                                        'fields': {
+                                            'type': 'object'
+                                        },
+                                        'filters': {
+                                            'type': 'object'
+                                        }
+                                    }
+                                },
+                                'style': 'form',
+                                'explode': true
+                            },
+                            {
+                                'description': 'event id',
+                                'in': 'path',
+                                'name': 'eventId',
+                                'required': true,
+                                'schema': {
+                                    'type': 'string'
+                                }
+                            },
+                            {
+                                'description': 'form id',
+                                'in': 'path',
+                                'name': 'id',
+                                'required': true,
+                                'schema': {
+                                    'type': 'string'
+                                }
+                            }
+                        ]
+                    }
+                },
+                '/events': {
+                    'post': {
+                        'consumes': [
+                            'multipart/form-data'
                         ],
-                        "requestBody": {
-                            "content": {
-                                "multipart/form-data": {
-                                    "schema": {
-                                        "type": "object",
-                                        "properties": {
-                                            "logo": {
-                                                "type": "string",
-                                                "format": "binary"
+                        'requestBody': {
+                            'content': {
+                                'multipart/form-data': {
+                                    'schema': {
+                                        'type': 'object',
+                                        'properties': {
+                                            'logo': {
+                                                'type': 'string',
+                                                'format': 'binary'
                                             },
-                                            "name": {
-                                                "type": "string"
+                                            'name': {
+                                                'type': 'string'
                                             },
-                                            "emailAlias": {
-                                                "type": "string"
+                                            'emailAlias': {
+                                                'type': 'string'
                                             },
-                                            "startDate": {
-                                                "type": "string"
+                                            'startDate': {
+                                                'type': 'string'
                                             },
-                                            "endDate": {
-                                                "type": "string"
+                                            'endDate': {
+                                                'type': 'string'
                                             },
-                                            "usersEmails": {
-                                                "type": "[string]"
+                                            'usersEmails': {
+                                                'type': '[string]'
                                             }
                                         }
                                     }
@@ -62,26 +146,26 @@ import ts from 'ttypescript/lib/typescript'
     }
 
     const routeOptions: RoutesConfig = {
-        "basePath": "/api/v1",
-        "entryFile": "./src/server.ts",
-        "routesDir": "./src/api/_auto",
-        "authenticationModule": "./src/middlewares/authentication.ts"
+        'basePath': '/api/v1',
+        'entryFile': './src/server.ts',
+        'routesDir': './src/api/_auto',
+        'authenticationModule': './src/middlewares/authentication.ts'
     }
 
     const ignore = [
-        "**/node_modules/**",
-        "./src/api/_auto/*",
-        "./src/static/*"
+        '**/node_modules/**',
+        './src/api/_auto/*',
+        './src/static/*'
     ]
 
     const compilerOptions = {
-        "moduleResolution": ts.ModuleResolutionKind.NodeJs,
-        "baseUrl": ".",
-        "paths": { //This has to be the same config as in tsconfig.json
-            "*": [
-                "src/*"
+        'moduleResolution': ts.ModuleResolutionKind.NodeJs,
+        'baseUrl': '.',
+        'paths': { //This has to be the same config as in tsconfig.json
+            '*': [
+                'src/*'
             ]
-        },
+        }
     }
 
     await generateSwaggerSpec(swaggerOptions, routeOptions, compilerOptions, ignore)
