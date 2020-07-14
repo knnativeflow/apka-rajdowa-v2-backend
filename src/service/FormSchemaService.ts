29e9c9565e0931b5d4156a47f9bdea20661f18d7
dimitor115 <dimitor115@gmail.com>
2020-07-14 22:31:05 +0200

    Remove mapping db fields to frontend keys

--- src/service/FormSchemaService.ts
+++ src/service/FormSchemaService.ts
@@ -6,13 +6,6 @@ import {byIdQuery} from 'common/utils'
 import Response from 'common/Response'
 import Exception from 'common/Exception'
 
-const DB_FIELD_KEYS_TO_FRONT_KEYS = {
-    name: 'label',
-    htmlType: 'type',
-    enum: 'values',
-    type: 'dataType'
-}
-
 async function create(
     { details, schema }: FormSchemaRequest,
     eventId: string
@@ -50,7 +43,7 @@ async function getPublic(slug: string): Promise<Response<FormSchemaDoc>> {
         throw Exception.fromMessage(`Not found schema by slug: ${slug}`, 404)
     } else {
         schema.structure = parseToPublic(schema.structure)
-        return new Response(_parseFormSchema(schema))
+        return new Response(schema)
     }
 }
 
@@ -61,7 +54,7 @@ async function getPrivate(slug: string): Promise<Response<FormSchemaDoc>> {
     if (!schema) {
         throw Exception.fromMessage(`Not found schema by slug: ${slug}`, 404)
     } else {
-        return new Response(_parseFormSchema(schema))
+        return new Response(schema)
     }
 }
 
@@ -87,29 +80,6 @@ function parseToPublic(schemaStructure: any): any {
                 }
                 : aggregate
         ), {})
-}
-
-function _parseFormSchema({ structure, ...rest }: FormSchemaDoc): FormSchemaDoc {
-    return {
-        ...rest,
-        structure: Object.keys(structure)
-            .reduce((obj, key) => {
-                obj[key] = _mapKeysToFrontFormat(structure[key][0] ? structure[key][0] : structure[key])
-                return obj
-            }, {})
-    } as any
-}
-
-function _mapKeysToFrontFormat(fieldObj: any): any {
-    return Object.keys(fieldObj)
-        .reduce((obj, key) => {
-            if (DB_FIELD_KEYS_TO_FRONT_KEYS[key]) {
-                obj[DB_FIELD_KEYS_TO_FRONT_KEYS[key]] = fieldObj[key]
-            } else {
-                obj[key] = fieldObj[key]
-            }
-            return obj
-        }, {})
 }
 
 export default {
